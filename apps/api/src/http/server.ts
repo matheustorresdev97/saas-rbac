5f3c00cfec7022abed70d1ee256f694acdce1406
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-09-06 09:15:27 -0300

    variaveis de ambiente e fluxo OAuth Github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import fastifyCors from "@fastify/cors";
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import { env } from '@repo/env'
 import fastify from "fastify";
 import {
   jsonSchemaTransform,
@@ -17,6 +18,7 @@ import { createAccount } from "./routes/auth/create-account";
 import { getProfile } from "./routes/auth/get-profile";
 import { requestPasswordRecover } from "./routes/auth/request-password-recover";
 import { resetPassword } from "./routes/auth/reset-password";
+import { authenticateWithGithub } from "./routes/auth/authenticate-with-github";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -32,6 +34,15 @@ app.register(fastifySwagger, {
       description: "Full-stack SaaS with multi-tenant & RBAC.",
       version: "1.0.0",
     },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
     servers: [],
   },
   transform: jsonSchemaTransform,
@@ -42,17 +53,18 @@ app.register(fastifySwaggerUI, {
 });
 
 app.register(fastifyJwt, {
-  secret: 'my-jwt-secret',
+  secret: env.JWT_SECRET,
 })
 
 app.register(fastifyCors);
 
 app.register(createAccount);
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log("HTTP server running!");
 });
