c25a848d88576e2053a8d331abafd82dfa2153e8
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-09-06 08:25:25 -0300

    auth user

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -3,6 +3,7 @@ import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
 import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app: FastifyInstance) {
@@ -10,8 +11,8 @@ export async function createAccount(app: FastifyInstance) {
     "/users",
     {
       schema: {
-        tags: ['Auth'],
-        summary: 'Create a new account',
+        tags: ["Auth"],
+        summary: "Create a new account",
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -29,10 +30,17 @@ export async function createAccount(app: FastifyInstance) {
       });
 
       if (userWithSameEmail) {
-        return reply
-          .status(400)
-          .send({ message: "User with same e-mail already exists." });
+        throw new BadRequestError('User with same e-mail already exists.')
       }
+
+      const [, domain] = email.split("@");
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      });
 
       const passwordHash = await hash(password, 6);
 
@@ -41,6 +49,13 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       });
 
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from "@fastify/cors";
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import fastify from "fastify";
@@ -8,12 +9,21 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { createAccount } from "../routes/auth/create-account";
+
+import { errorHandler } from '@/http/error-handler'
+import { authenticateWithPassword } from '@/http/routes/auth/authenticate-with-password'
+
+import { createAccount } from "./routes/auth/create-account";
+import { getProfile } from "./routes/auth/get-profile";
+import { requestPasswordRecover } from "./routes/auth/request-password-recover";
+import { resetPassword } from "./routes/auth/reset-password";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifySwagger, {
   openapi: {
@@ -31,9 +41,17 @@ app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
 });
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors);
 
 app.register(createAccount);
+app.register(authenticateWithPassword)
+app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
