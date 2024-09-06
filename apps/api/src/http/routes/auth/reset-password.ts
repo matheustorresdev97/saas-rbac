7fdd3f700bfe3bf91370e6d609038fee6b292670
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-09-06 11:01:52 -0300

    finish back-end

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -1,18 +1,18 @@
-import { hash } from 'bcryptjs'
-import type { FastifyInstance } from 'fastify'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { hash } from "bcryptjs";
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
 
-import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
-import { prisma } from '@/lib/prisma'
+import { UnauthorizedError } from "@/http/routes/_errors/unauthorized-error";
+import { prisma } from "@/lib/prisma";
 
 export async function resetPassword(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
-    '/password/reset',
+    "/password/reset",
     {
       schema: {
-        tags: ['Auth'],
-        summary: 'Get authenticated user profile',
+        tags: ["Auth"],
+        summary: "Get authenticated user profile",
         body: z.object({
           code: z.string(),
           password: z.string().min(6),
@@ -23,28 +23,35 @@ export async function resetPassword(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      const { code, password } = request.body
+      const { code, password } = request.body;
 
       const tokenFromCode = await prisma.token.findUnique({
         where: { id: code },
-      })
+      });
 
       if (!tokenFromCode) {
-        throw new UnauthorizedError()
+        throw new UnauthorizedError();
       }
 
-      const passwordHash = await hash(password, 6)
+      const passwordHash = await hash(password, 6);
 
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ]);
 
-      return reply.status(204).send()
-    },
-  )
+      return reply.status(204).send();
+    }
+  );
 }
