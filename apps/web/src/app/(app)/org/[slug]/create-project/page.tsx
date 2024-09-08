23709222d3730dabcca3bb71ce67d79ce6d2f33e
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-09-08 19:57:47 -0300

    navegação por abas e permissoes

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -12,13 +12,10 @@ export default async function CreateProject() {
     redirect('/')
   }
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
 
-        <ProjectForm />
-      </main>
+      <ProjectForm />
     </div>
   )
 }
